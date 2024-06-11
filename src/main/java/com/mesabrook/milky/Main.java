60089cc1152c1405796a1b41e3907fb59e1d5201
Christopher <supersoftware1234@gmail.com>
2024-06-11 14:37:04 -0400

    Begin

---
+++ src/main/java/com/mesabrook/milky/Main.java
@@ -0,0 +1,31 @@
+package com.mesabrook.milky;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Main.MODID, name = Main.NAME, version = Main.VERSION)
+public class Main
+{
+    public static final String MODID = "milky";
+    public static final String MOD_PREFIX = MODID + ":";
+    public static final String NAME = "Milky";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    	
+    }
+}
