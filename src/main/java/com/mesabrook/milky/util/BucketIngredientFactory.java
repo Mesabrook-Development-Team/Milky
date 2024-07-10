fd9345c0cf56ceaf677bf2eba85d18ca4aad1b66
Adam Clussman <cnwaj@hotmail.com>
2024-07-10 10:04:00 -0500

    Make Bucket Ingredient use NBT

--- src/main/java/com/mesabrook/milky/util/BucketIngredientFactory.java
+++ src/main/java/com/mesabrook/milky/util/BucketIngredientFactory.java
@@ -3,9 +3,11 @@ package com.mesabrook.milky.util;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSyntaxException;
 
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.JsonUtils;
 import net.minecraftforge.common.crafting.IIngredientFactory;
+import net.minecraftforge.common.crafting.IngredientNBT;
 import net.minecraftforge.common.crafting.JsonContext;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidRegistry;
@@ -24,7 +26,18 @@ public class BucketIngredientFactory implements IIngredientFactory {
 		}
 		
 		FluidStack fluidStack = new FluidStack(fluid, 1000);
-		return Ingredient.fromStacks(FluidUtil.getFilledBucket(fluidStack));
+		return NBTIngredient.fromStack(FluidUtil.getFilledBucket(fluidStack));
 	}
 
+	private static class NBTIngredient extends IngredientNBT
+	{
+		protected NBTIngredient(ItemStack stack) {
+			super(stack);
+		}
+		
+		public static NBTIngredient fromStack(ItemStack stack)
+		{
+			return new NBTIngredient(stack);
+		}
+	}
 }
